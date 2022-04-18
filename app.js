ca6bc7c3a5e8a766b2f327eb1d7e6c47a9feda8f
Lashawn <lashawnwood23@gmail.com>
2022-04-18 19:34:22 -0400

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+const myArray = [];
+for (var i = 1; i <= 10; i++) {
+    myArray.push(i);
+ }
+ const ourArray = [];
+var i = 10;
+
+while (i < 1) {
+  ourArray.push(i);
+  i++;
+}
+const myArray = [];
+for (var i = 13; i <= 19; i++) {
+    myArray.push(i);
+ }
+ const ourArray = [];
+
+for (var i = 15; i > 0; i -= 2) {
+  ourArray.push(i);
+}
